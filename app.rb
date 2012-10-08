af674680662b1b3ddcee80505dff32b7e5bacbc6
José Mota <josemota.net@gmail.com>
2012-10-08 16:21:50 +0100

    Templates.

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require "sinatra/base"
 
+IMAGES = [
+  { title: "Utopia"     , url: "http://fantasyartdesign.com/free-wallpapers/imgs/mid/68landscapes04-m241.jpg" },
+  { title: "Alaska"     , url: "http://www.davidsfotos.com/LANDSCAPESpage_files/LANDSCAPES2.jpg" },
+  { title: "The Unknown", url: "http://www.beautifullife.info/wp-content/uploads/2010/12/31/the-unknown.jpg" },
+]
+
 class App < Sinatra::Base
+
+  get "/images" do
+    @images = IMAGES
+    erb :images
+  end
+
+  get "/images/:index" do |index|
+    index = index.to_i
+    @image = IMAGES[index]
+
+    haml :"images/show", layout: true
+  end
+
   get "/" do
     "Hello World!"
   end
